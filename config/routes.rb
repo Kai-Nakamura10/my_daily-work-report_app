89422f293d21e3c3b3f718cb33143be5f9e20c0d
kainakamura <yamadeko2761@gmail.com>
2025-06-13 17:49:58 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  root "homes#index"
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  get    'signup', to: 'users#new'
+  post   'signup', to: 'users#create'
   resources :reports
-  root "reports#index"
 end
