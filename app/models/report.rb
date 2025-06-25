1bb8d76d9197d54e0f57e37ab804471d830c8bf8
kainakamura <yamadeko2761@gmail.com>
2025-06-25 18:07:10 +0900

    業務日報機能

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,7 @@
 class Report < ApplicationRecord
+  belongs_to :user
+
+  validates :title, :body, presence: true
+  validates :body, length: { maximum: 400 }
+  validates :title, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :reports, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 255 }
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
